6824351e0a11d9a613b158997fcc4903135ba059
Viraj Jayasiri <viraj.jayasiri.work@gmail.com>
2026-02-04 23:17:09 +0530

    Add Contact and Footer components to enhance website functionality

---
+++ app/components/Home/Contact/Contact.tsx
@@ -0,0 +1,55 @@
+import { BiEnvelope, BiLogoFacebook, BiLogoInstagram, BiLogoTwitter, BiLogoYoutube, BiMap, BiPhone, BiX } from 'react-icons/bi'
+
+const Contact = () => {
+  return (
+    <div className='pt-16 pb-16'>
+      <div className='w-[90%] md:w-[80%] lg:w-[70%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
+         {/* Text Content */}
+         <div>
+            <h1 className='text-3xl sm:text-4xl lg:text-5xl font-bold text-[#1D3557]'>Schedule a Meeting with Me to See if I can Help</h1>
+            <p className='text-[#E63946] mt-6 text-base sm:text-lg'>Reach out to me today & let's discuss how I can help you achieve your goals.</p>
+            {/* Info */}
+            <div className='mt-7'>
+               <div className='flex items-center space-x-3 mb-4'>
+                  <BiPhone className='w-9 h-9 text-[#212529]'/>
+                  <p className='text-xl font-bold text-[#212529]'>+94 76 870 8384</p>
+               </div>
+               <div className='flex items-center space-x-3 mb-4'>
+                  <BiEnvelope className='w-9 h-9 text-[#212529]'/>
+                  <p className='text-xl font-bold text-[#212529]'>viraj.jayasiri@outlook.com</p>
+               </div>
+               <div className='flex items-center space-x-3 mb-4'>
+                  <BiMap className='w-9 h-9 text-[#212529]'/>
+                  <p className='text-xl font-bold text-[#212529]'>Mount Lavinia, Sri Lanka</p>
+               </div>
+            </div>
+            {/* Social Media Icons */}
+            <div className='flex items-center mt-8 space-x-3'>
+               <div className='w-14 h-14 bg-[#457B9D] rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-blue-700 transition-all duration-300'>
+                  <BiLogoFacebook className='text-white w-6 h-6'/>
+               </div>
+               <div className='w-14 h-14 bg-[#457B9D] rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-red-600 transition-all duration-300'>
+                  <BiLogoYoutube className='text-white w-6 h-6'/>
+               </div>
+               <div className='w-14 h-14 bg-[#457B9D] rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-sky-400 transition-all duration-300'>
+                  <BiLogoTwitter className='text-white w-6 h-6'/>
+               </div>
+               <div className='w-14 h-14 bg-[#457B9D] rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-pink-600 transition-all duration-300'>
+                  <BiLogoInstagram className='text-white w-6 h-6'/>
+               </div>
+            </div>
+         </div>
+         {/* Form */}
+         <div className='md:p-10 p-5 bg-[#A8DADC] rounded-lg'>
+            <input type="text" placeholder='Name' className='px-4 py-3.5 bg-[#457B9D] text-white outline-none rounded-md w-full placeholder:text-white/70'/>
+            <input type="email" placeholder='Email Address' className='px-4 py-3.5 mt-6 bg-[#457B9D] text-white outline-none rounded-md w-full placeholder:text-white/70'/>
+            <input type="text" placeholder='Phone Number' className='px-4 py-3.5 mt-6 bg-[#457B9D] text-white outline-none rounded-md w-full placeholder:text-white/70'/>
+            <textarea name="message" placeholder="Your Message" className='px-4 py-3.5 mt-6 bg-[#457B9D] text-white outline-none rounded-md w-full placeholder:text-white/70 h-[10rem]'></textarea>
+            <button className='mt-8 px-12 py-4 bg-[#E63946] hover:bg-[#000000] transition-all duration-300 cursor-pointer text-white rounded-full'>Send Messsage</button>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ app/components/Home/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#1D3557] p-7'>
+      <h1 className='text-white text-center text-lg'>&copy; Viraj Jayasiri 2025 - Personal Portfolio WebSite. All rights reserved.</h1>
+    </div>
+  )
+}
+
+export default Footer
